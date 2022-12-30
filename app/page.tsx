69c90c15b3c4a39c50e4398fa1eeee95ab3e142c
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-12-30 21:02:06 +0100

    blog setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-white'>
+        <h1 className='text-6xl'>Welcome to the Blog</h1>
+    </div>
+  )
+}
+
+export default page
