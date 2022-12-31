6dbce88e36b951be6b0d6b27bc62269542d5e98d
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-12-31 23:35:34 +0100

    typings

---
+++ components/Bloglist.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Post } from '../typings';
+
+type Props = { posts: Post[] };
+const Bloglist = ({ posts }: Props) => {
+  return <div>Bloglist</div>;
+};
+
+export default Bloglist;
